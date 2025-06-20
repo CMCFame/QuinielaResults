a1cde8d05cc3edf89dc1a442428b70d61a9b82c7
CMCFame <jetronico@gmail.com>
2025-06-20 06:41:41 -0600

    Update __init__.py

--- portfolio/__init__.py
+++ portfolio/__init__.py
@@ -1,2 +1,4 @@
+# progol_optimizer/portfolio/__init__.py
 from .core_generator import CoreGenerator
 from .satellite_generator import SatelliteGenerator
+# No importar el optimizador aquí para romper el ciclo
